5f53cbb7b13136c8d8c2c0ceb8a3d1f0be170fcd
Damianos Sotirakis <dansotirakis18@gmail.com>
2019-06-16 03:30:18 -0300

    ReactFuncional

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Routes from './routes';
+import {BrowserRouter} from 'react-router-dom';
+
+import './global.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
